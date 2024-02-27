7e123e7a2f9c8ae99ba8e1ff47636ccabb60580a
Parallels <kasperisager@hey.com>
2024-02-27 10:08:14 +0100

    Respect `ARGV_DESTINATION` and clean up asset download

--- app-image.cmake
+++ app-image.cmake
@@ -1,35 +1,75 @@
 set(app_image_module_root ${CMAKE_CURRENT_LIST_DIR})
 
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
-  file(
-    DOWNLOAD
-      https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-aarch64.AppImage
-      "${CMAKE_CURRENT_BINARY_DIR}/appimagetool.AppImage"
-    EXPECTED_HASH SHA256=334e77beb67fc1e71856c29d5f3f324ca77b0fde7a840fdd14bd3b88c25c341f
-  )
+  set(app_image_arch aarch64)
+elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
+  set(app_image_arch x86_64)
 else()
-  file(
-    DOWNLOAD
-      https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
-      "${CMAKE_CURRENT_BINARY_DIR}/appimagetool.AppImage"
-    EXPECTED_HASH SHA256=df3baf5ca5facbecfc2f3fa6713c29ab9cefa8fd8c1eac5d283b79cab33e4acb
-  )
+  message(FATAL_ERROR "Unsupported AppImage architecture '${CMAKE_SYSTEM_PROCESSOR}'")
 endif()
 
-file(CHMOD "${CMAKE_CURRENT_BINARY_DIR}/appimagetool.AppImage" PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
+set(app_image_download_base "https://github.com/AppImage/AppImageKit/releases/download")
 
-set(app_image_tool "${CMAKE_CURRENT_BINARY_DIR}/appimagetool.AppImage")
+set(app_image_download_release "13")
+
+function(download_app_image_run)
+  cmake_parse_arguments(
+    PARSE_ARGV 0 ARGV "" "DESTINATION" ""
+  )
+
+  cmake_path(ABSOLUTE_PATH ARGV_DESTINATION BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" NORMALIZE)
+
+  file(
+    DOWNLOAD "${app_image_download_base}/${app_image_download_release}/AppRun-${app_image_arch}"
+    "${ARGV_DESTINATION}"
+    LOG log
+  )
+
+  file(CHMOD "${ARGV_DESTINATION}" PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
+
+  message(STATUS "${log}")
+endfunction()
+
+function(download_app_image_tool)
+  cmake_parse_arguments(
+    PARSE_ARGV 0 ARGV "" "DESTINATION" ""
+  )
+
+  cmake_path(ABSOLUTE_PATH ARGV_DESTINATION BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" NORMALIZE)
+
+  file(
+    DOWNLOAD "${app_image_download_base}/${app_image_download_release}/appimagetool-${app_image_arch}.AppImage"
+    "${ARGV_DESTINATION}"
+    LOG log
+  )
+
+  file(CHMOD "${ARGV_DESTINATION}" PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
+
+  message(STATUS "${log}")
+endfunction()
 
 function(add_app_image target)
   cmake_parse_arguments(
-    PARSE_ARGV 1 ARGV "" "DESTINATION;NAME;ICON;TARGET;EXECUTABLE" "CATEGORIES"
+    PARSE_ARGV 1 ARGV "" "DESTINATION;NAME;ICON;TARGET;EXECUTABLE;APP_DIR" "CATEGORIES"
   )
 
+  download_app_image_tool(DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/appimagetool.AppImage")
+
+  set(app_image_tool "${CMAKE_CURRENT_BINARY_DIR}/appimagetool.AppImage")
+
 	if(NOT ARGV_DESTINATION)
-		set(ARGV_DESTINATION ${ARGV_NAME}.AppDir)
+		set(ARGV_DESTINATION ${ARGV_NAME}.AppImage)
 	endif()
 
   cmake_path(ABSOLUTE_PATH ARGV_DESTINATION BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" NORMALIZE)
+
+  if(ARGV_APP_DIR)
+    cmake_path(ABSOLUTE_PATH ARGV_APP_DIR BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" NORMALIZE)
+  else()
+    cmake_path(REMOVE_EXTENSION ARGV_DESTINATION LAST_ONLY OUTPUT_VARIABLE ARGV_APP_DIR)
+
+    string(APPEND ARGV_APP_DIR ".AppDir")
+  endif()
 
 	if(ARGV_ICON)
 	  cmake_path(ABSOLUTE_PATH ARGV_ICON NORMALIZE)
@@ -45,21 +85,7 @@ function(add_app_image target)
 		cmake_path(GET ARGV_EXECUTABLE FILENAME ARGV_EXECUTABLE_NAME)
   endif()
 
-	if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
-	  file(
-	    DOWNLOAD
-	      https://github.com/AppImage/AppImageKit/releases/download/13/AppRun-aarch64
-	      "${ARGV_DESTINATION}/AppRun"
-	    EXPECTED_HASH SHA256=9214c4c1f7a3cdd77f8d558c2039230a322469f8aaf7c71453eeaf1f2f33d204
-	  )
-	else()
-	  file(
-	    DOWNLOAD
-	      https://github.com/AppImage/AppImageKit/releases/download/13/AppRun-x86_64
-	      "${ARGV_DESTINATION}/AppRun"
-	    EXPECTED_HASH SHA256=fd0e2c14a135e7741ef82649558150f141a04c280ed77a5c6f9ec733627e520e
-	  )
-	endif()
+  download_app_image_run(DESTINATION "${ARGV_APP_DIR}/AppRun")
 
 	list(JOIN ARGV_CATEGORIES ";" ARGV_CATEGORIES)
 
@@ -67,23 +93,28 @@ function(add_app_image target)
 
   string(CONFIGURE "${template}" template)
 
-  file(GENERATE OUTPUT "${ARGV_DESTINATION}/${ARGV_NAME}.desktop" CONTENT "${template}" NEWLINE_STYLE UNIX)
+  file(GENERATE OUTPUT "${ARGV_APP_DIR}/${ARGV_NAME}.desktop" CONTENT "${template}" NEWLINE_STYLE UNIX)
 
 	if(ARGV_ICON)
-		configure_file("${ARGV_ICON}" "${ARGV_DESTINATION}/icon.png" COPYONLY)
+		configure_file("${ARGV_ICON}" "${ARGV_APP_DIR}/icon.png" COPYONLY)
 	endif()
 
 	add_custom_target(
 		${target}_bin
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${ARGV_EXECUTABLE}" "${ARGV_DESTINATION}/usr/bin/${ARGV_EXECUTABLE_NAME}"
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${ARGV_EXECUTABLE}" "${ARGV_APP_DIR}/usr/bin/${ARGV_EXECUTABLE_NAME}"
 	)
 
 	list(APPEND ARGV_DEPENDS ${target}_bin)
 
-	add_custom_target(
-		${target}
-		ALL
-		COMMAND "${app_image_tool}" "${ARGV_DESTINATION}"
+	add_custom_command(
+    OUTPUT "${ARGV_DESTINATION}"
+		COMMAND "${app_image_tool}" --no-appstream "${ARGV_APP_DIR}" "${ARGV_DESTINATION}"
 		DEPENDS ${ARGV_DEPENDS}
 	)
+
+  add_custom_target(
+    ${target}
+    ALL
+    DEPENDS "${ARGV_DESTINATION}"
+  )
 endfunction()
